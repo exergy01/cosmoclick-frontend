f78c76c3d6936a4ab8662cc69721ad196a6a9efd
exergy01 <contora29@gmail.com>
2025-05-07 08:37:34 +0300

    Fix imports to use ./context/PlayerContext

--- src/components/ResourceButtons.tsx
+++ src/components/ResourceButtons.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from 'axios';
-import { usePlayer } from '../context/PlayerContext';
+import { usePlayer } from './context/PlayerContext';
 import { useNavigate, useLocation } from 'react-router-dom';
 import { asteroidData } from '../data/shopDataSystem1';
 
