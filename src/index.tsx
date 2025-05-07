22f9ba73da18ba758ceb5bcae1239e9c8851753a
exergy01 <contora29@gmail.com>
2025-05-07 09:30:32 +0300

    Fix PlayerContext import path in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { PlayerProvider } from '../context/PlayerContext';
+import { PlayerProvider } from './context/PlayerContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
