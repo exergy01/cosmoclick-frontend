bf82497f6ba7de0f719ebe35d5fe8d66225683f3
exergy01 <contora29@gmail.com>
2025-06-18 16:24:00 +0300

    test

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,5 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { I18nextProvider } from 'react-i18next';
-import i18n from './i18n';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <Suspense fallback={<div>Loading...</div>}>
-      <I18nextProvider i18n={i18n}>
-        <Router>
-          <App />
-        </Router>
-      </I18nextProvider>
-    </Suspense>
-  </React.StrictMode>
-);
-
-reportWebVitals();
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(React.createElement('h1', {style: {color: 'red', fontSize: '50px'}}, 'TEST WORKS!'));
