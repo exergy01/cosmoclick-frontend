da172acbc727c1a9406a6c672ba9b494bb88ed0c
exergy01 <contora29@gmail.com>
2025-06-18 17:06:58 +0300

    🧪 Minimal React test

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(React.createElement('h1', {style: {color: 'red', fontSize: '50px'}}, 'TEST WORKS!'));
+const App = () => {
+  return React.createElement('div', {}, 
+    React.createElement('h1', {style: {color: 'green', fontSize: '40px'}}, 'ROUTER TEST WORKS!')
+  );
+};
+
+const AppWithRouter = () => {
+  return React.createElement(Router, {}, React.createElement(App));
+};
+
+const container = document.getElementById('root') as HTMLElement;
+const root = createRoot(container);
+root.render(React.createElement(AppWithRouter));
