0f950ae35cf0906a13e7c91202ea06c9f4ceb072
exergy01 <contora29@gmail.com>
2025-06-19 07:35:16 +0300

    1

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { I18nextProvider } from 'react-i18next';
+import i18n from './i18n';
 
 const App = () => {
   return React.createElement('div', {}, 
-    React.createElement('h1', {style: {color: 'green', fontSize: '40px'}}, 'ROUTER TEST WORKS!')
+    React.createElement('h1', {style: {color: 'orange', fontSize: '40px'}}, 'I18N TEST WORKS!')
   );
 };
 
-const AppWithRouter = () => {
-  return React.createElement(Router, {}, React.createElement(App));
+const AppWithAll = () => {
+  return React.createElement(I18nextProvider, {i18n}, 
+    React.createElement(Router, {}, 
+      React.createElement(App)
+    )
+  );
 };
 
 const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
-root.render(React.createElement(AppWithRouter));
+root.render(React.createElement(AppWithAll));
